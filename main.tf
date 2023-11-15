638bb6abd76bce39cac813be0e39ac3483e400ea
Owen Tagg <119607182+Zowen03@users.noreply.github.com>
2023-11-15 21:39:51 +0000

    fixes

--- main.tf
+++ main.tf
@@ -36,32 +36,31 @@ resource "aws_security_group" "web" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "web_http_in {
+resource "aws_security_group_rule" "web_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "web_https_in {
+
+resource "aws_security_group_rule" "web_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "web_everything_out {
+
+resource "aws_security_group_rule" "web_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.web.id
 }
